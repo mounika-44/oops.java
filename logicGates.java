323aed6bf355a25dbfdbb2cb086e8ec724ce5bcd
mounika-44 <24wh1a05t1@bvrithyderabad.edu.in>
2025-09-08 19:43:55 +0530

    Create logicGates.java

---
+++ logicGates.java
@@ -0,0 +1,9 @@
+public class LogicGates {
+    public static void main(String[] args) {
+        boolean a = true, b = false;
+
+        System.out.println("a AND b: " + (a && b));
+        System.out.println("a OR b: " + (a || b));
+        System.out.println("NOT a: " + (!a));
+    }
+}

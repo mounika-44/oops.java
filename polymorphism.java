39668f5f4e86bde510a81ff0f6944e6adf687e50
mounika-44 <24wh1a05t1@bvrithyderabad.edu.in>
2025-09-08 19:47:35 +0530

    Create polymorphism.java

---
+++ polymorphism.java
@@ -0,0 +1,29 @@
+class Shape {
+    void draw() {
+        System.out.println("Drawing Shape");
+    }
+}
+
+class Circle extends Shape {
+    void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+class Square extends Shape {
+    void draw() {
+        System.out.println("Drawing Square");
+    }
+}
+
+public class Polymorphism {
+    public static void main(String[] args) {
+        Shape s;
+
+        s = new Circle();
+        s.draw();
+
+        s = new Square();
+        s.draw();
+    }
+}
